691df1181056834ed998711ab16634499443d74c
Ye Jin Jeon <figure97@naver.com>
2022-11-16 17:59:36 -0800

    edit hifigan path

--- utils/model.py
+++ utils/model.py
@@ -55,14 +55,14 @@ def get_vocoder(config, device):
         vocoder.mel2wav.eval()
         vocoder.mel2wav.to(device)
     elif name == "HiFi-GAN":
-        with open("C:/Users/samsung/Git/FastSpeech2/hifigan/config.json", "r") as f:
+        with open("/home/yejeon/CSE291B_Fastspeech2/hifigan/config.json", "r") as f:
             config = json.load(f)
         config = hifigan.AttrDict(config)
         vocoder = hifigan.Generator(config)
         if speaker == "LJSpeech":
-            ckpt = torch.load("C:/Users/samsung/Git/FastSpeech2/hifigan/generator_LJSpeech.pth.tar")
+            ckpt = torch.load("/home/yejeon/CSE291B_Fastspeech2/hifigan/generator_LJSpeech.pth.tar")
         elif speaker == "universal":
-            ckpt = torch.load("C:/Users/samsung/Git/FastSpeech2/hifigan/generator_universal.pth.tar", map_location=torch.device('cpu'))
+            ckpt = torch.load("/home/yejeon/CSE291B_Fastspeech2/hifigan/generator_universal.pth.tar", map_location=torch.device('cpu'))
         vocoder.load_state_dict(ckpt["generator"])
         vocoder.eval()
         vocoder.remove_weight_norm()
